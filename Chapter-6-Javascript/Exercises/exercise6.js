0a811785bd5d8a572a71991ab1f2f1ff243d2650
joseshaan123 <144330422+joseshaan123@users.noreply.github.com>
2024-04-16 00:18:24 +0400

    Add files via upload

---
+++ Chapter-6-Javascript/Exercises/exercise6.js
@@ -0,0 +1,7 @@
+function totalFunction() {
+    let normalamount = document.getElementById("Amount").value;
+    let litres= document.getElementById("litres").value;
+    c= normalamount*litres
+    let total =document.getElementById("TotalAmount").value=c
+    
+} 
